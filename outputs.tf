c989a510cef70b4371228c795a50c7118d5aae11
rezachi <rezachi@yahoo.com>
2025-11-04 19:28:07 -0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "instance_ami" {
-  value = aws_instance.blog.ami
+  value = aws_instance.blog.ami[count.index]
 }
 
 output "instance_arn" {
-  value = aws_instance.blog.arn
+  value = aws_instance.blog.arn[count.index]
 }
